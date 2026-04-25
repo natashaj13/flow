1b449498d18910ab34f664c47a6411fa04a6c311
Shreya Chati <shreyachati7@gmail.com>
2026-04-24 21:03:15 -0700

    exportable

--- chrome.js
+++ chrome.js
@@ -1,4 +1,12 @@
 const CDP = require('chrome-remote-interface');
-tabs = CDP.List({ host: 'localhost', port: 9222 }).then(tabs => {
-    console.log(tabs);
-});
+
+async function getTabs() {
+    const tabs = await CDP.List({ host: 'localhost', port: 9222 });
+    return tabs.filter(t => t.type === 'page');
+}
+
+module.exports = { getTabs };
+
+// usage: 
+// const{getTabs} = require{'./chrome'};
+// const tabs = await getTabs();
